a00d3534a1f80792c3b63478d22ab39406ca3649
RichieEar <35822403+RichieEar@users.noreply.github.com>
2018-10-22 23:54:33 -0700

    Add files via upload

--- edu.package3/Phone.java
+++ edu.package3/Phone.java
@@ -1,4 +1,3 @@
-package edu.project3;
 
 import java.util.Date;
 import java.util.InputMismatchException;
@@ -11,18 +10,15 @@ public class Phone {
 		Log log = new Log();
 		Favorites favorites = new Favorites();
 		PhoneBook phone = new PhoneBook();
-			//Big loop the runs the overall menu that can be quit through options
+		System.out.println("Would you like to access your phone book? 'Yes' or 'No' ");
+		
 			while(true) {
 				System.out.println("Please enter the number(1-5) of what you want to access:\n1. Phone book\n2. Favorites\n3. Calls\n4. Call History\n5: Exit phone");
-				//This is used to split the user into the seperate streams to the different menus
 				String nextInput = scan.nextLine();
-				//Phonebook menu
 				if (nextInput.equals("1")) {
 					while(true) {
 						System.out.println("Welcome to the Phonebook, would you like to:\n1. Add a contact\n2. Delete a contact\n3. Edit a contact\n4. Alphabetize and Display phonebook\n5: Exit Phonebook");
-						//Used to navigate phonebook
 						String nextInput1 = scan.nextLine();
-						//Process of adding a contact.
 						if(nextInput1.equals("1")) {
 							System.out.println("What is the name of the contact you wish to add?");
 							String name = scan.nextLine();
@@ -35,7 +31,6 @@ public class Phone {
 							
 							phone.add(new Contact(name,number,email,notes));
 						}
-						//Remove contact
 						else if (nextInput1.equals("2")) {
 							try{
 								System.out.println("What is the name of the contact you wish to remove?");
@@ -45,7 +40,6 @@ public class Phone {
 								System.out.println("Name not found ensure you are spelling the name correct and that the contact you are looking for exists then try again.");
 							}
 						}
-						//Edit contact
 						else if (nextInput1.equals("3")) {
 							try{
 								System.out.println("What is the name of the contact you wish to edit?");
@@ -63,28 +57,21 @@ public class Phone {
 								System.out.println("Name not found ensure you are spelling the name correct and that the contact you are looking for exists then try again.");
 							}
 						}
-						//Sort and display contacts
 						else if (nextInput1.equals("4")) {
 							phone.sort();
 							phone.display();
 						}
-						//Exits to main menu
 						else if (nextInput1.equals("5")) {
 							break;
 						}
-						//Prints if they entered anything but a number from 1 to 5
 						else {
 							System.out.println("Please enter a number from 1 to 5");
 						}
 					}
 				}
-				//Favorites menu
 				else if (nextInput.equals("2")) {
 					System.out.println("Welcome to Favorites, would you like to:\n1. Add a Favorite\n2. Remove a Favorite\n3.Change prefeerence\n4.Display a Favorite\n5: Exit Favorites");
-					//Used to navigate favorites
-					String favNav = scan.nextLine();
-					//Adds a favorite
-					if (favNav.equals("1")) {
+					if (nextInput.equals("1")) {
 						System.out.println("What is the name of the contact you wish to add?");
 						String name = scan.nextLine();
 						System.out.println("What is the phone number of the contact you wish to add?");
@@ -97,8 +84,7 @@ public class Phone {
 						String address = scan.nextLine();
 						favorites.add(address,name,number, email, notes);
 					}
-					//Removes a favorite
-					else if (favNav.equals("2")) {
+					else if (nextInput.equals("2")) {
 						try {
 							System.out.println("What is the name of the Favorite you wish to remove?");
 							String name = scan.nextLine();
@@ -107,8 +93,7 @@ public class Phone {
 							System.out.println("Name not found ensure you are spelling the name correct and that the contact you are looking for exists then try again.");
 						}
 					}
-					//Changes preference of favorite.
-					else if (favNav.equals("3")) {
+					else if (nextInput.equals("3")) {
 						try {
 							System.out.println("Enter the name of the Favorite you wish to change");
 							String name = scan.nextLine();
@@ -120,8 +105,7 @@ public class Phone {
 							System.out.println("Make sure you entered a number between 1 and 5 and that there are at least as many favorites you are trying to add.");
 						}
 					}
-					//Displays contact
-					else if (favNav.equals("4")) {
+					else if (nextInput.equals("4")) {
 						try {
 							System.out.println("Enter the Number of the contact you wish to display.");
 							int index = Integer.valueOf(scan.nextLine());
@@ -131,20 +115,15 @@ public class Phone {
 							System.out.println("Make sure you entered a number between 1 and 5 and that there are at least as many favorites you are trying to add.");
 						}
 					}
-					//Exits to main menu.
-					else if (favNav.equals("5")) {
+					else if (nextInput.equals("5")) {
 						break;
 					}
 				}
-				//Call center
 				else if (nextInput.equals("3")) {
-					//Loop to keep the call entry section running
 					while(true){
 						System.out.println("Enter a phone number, a name or a Favorite 1-5, to make or recieve a call. Type \"menu\" to go back to menu");
-						//Used to determine what kind of Id the user is entering
 						String num = scan.nextLine();
-						//If the ID is of length one and a number then it is a favorite
-						if(num.length()==1&&Character.isDigit(num.charAt(0))) {
+						if(num.length()==1) {
 							System.out.println("Is the call comming in or out? Type \"in\" for in and \"out\", for out.");
 							String IO = scan.nextLine();
 							if(IO.equals("in")) {
@@ -154,9 +133,7 @@ public class Phone {
 							}
 							Date date = new Date();
 							log.add(new Record(favorites.fav.get(Integer.valueOf(num)).getName(),date.toString(),IO));
-						}
-						//If it's the same length as a phonenumber and the first character is a number then it's treated as a phonenumber
-						else if(num.length()==10&&Character.isDigit(num.charAt(0))) {
+						}else if(num.length()==10&&Character.isDigit(num.charAt(0))) {
 							System.out.println("Is the call comming in or out? Type \"in\" for in and \"out\", for out.");
 							
 							String IO = scan.nextLine();
@@ -171,9 +148,7 @@ public class Phone {
 							pNum.insert(4,")");
 							pNum.insert(8,"-");
 							log.add(new Record(pNum.toString(),date.toString(),IO));
-						}
-						//If the first character is not a number it's assumed to be a name and looks for it in the phonebook.
-						else if(!Character.isDigit(num.charAt(0))) {
+						}else if(!Character.isDigit(num.charAt(0))) {
 							if(num.equals("menu")) {
 								break;
 							}
@@ -200,24 +175,19 @@ public class Phone {
 						}
 					}
 				}
-				//Call history
 				else if (nextInput.equals("4")) {
-					//Displays log
 					if(log.display()) {
 						System.out.println("Do you wish to see more information?(Y/N)");
 						String temp = scan.nextLine();
-						//Shows duplicates
 						if(temp.toLowerCase().equals("y")) {
 							log.showDupe();
 						}
 					}
 				}
-				//Quits to main menu
 				else if (nextInput.equals("5")) {
 					break;
 				}
 				
-				//if you didn't enter a number 1-5 it re-loops and asks you to use a number 1-5.
 				else {
 					System.out.println("Please enter a number from 1 to 5");
 					//continue;
